faa6304da553800beec0910823c8751e73ebc53d
jek <jakazzz19@gmail.com>
2024-09-14 12:30:24 +0700

    add axios and sass compiler

---
+++ src/pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+  <ol>
+    <li v-for="(val, i) in data.data" :key="i">{{ val.title }}</li>
+  </ol>
+</template>
+<script setup>
+import { ref, onMounted } from "vue";
+import axios from "axios";
+const data = ref({});
+
+onMounted(async () => {
+  data.value = await axios.get("https://jsonplaceholder.typicode.com/posts");
+  console.log(data.value);
+});
+</script>
+<style lang="scss">
+ol{
+  margin: 0;
+
+  li{
+    list-style-position: inside;
+  }
+}
+</style>
